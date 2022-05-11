28d86618729c37bd472a4334e25307bca65f271a
davicami <100150296+davicami@users.noreply.github.com>
2022-05-11 16:37:37 +0200

    Added features addBook, removeBook

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-let myLibrary = [];
+let library = [];
 
-function Book(title, author, pagesNumber, read) {
-    this.title = title,
-        this.author = author,
-        this.pageNumbers = pagesNumber;
-    this.read = read;
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
 
     this.info = function () {
-        return title + " by " + author + ", " + pagesNumber + " pages, " +
-            (read ? " read" : "not read yet")
-
+        return title + " by " + author + ", " + pages + " pages, " +
+            (isRead ? " read" : "not read yet");
     }
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+    library.push(book);
 }
 
 hobbit = new Book("The Hobbit", "Tolkien", 288, false);
@@ -25,19 +24,24 @@ console.log("Lista libri: ");
 addBookToLibrary(hobbit);
 addBookToLibrary(antifragile);
 
-myLibrary.forEach(book => {
+library.forEach(book => {
     console.log(book.info());
 });
 
 
 // user interface
-
+const addBookBtn = document.getElementById('addBookBtn')
+const addBookModal = document.getElementById('addBookModal')
+const errorMsg = document.getElementById('errorMsg')
+const overlay = document.getElementById('overlay')
 const addBookForm = document.getElementById('addBookForm');
 const booksGrid = document.getElementById('books-grid');
 
+
+
 const updateBooksGrid = () => {
     resetBooksGrid()
-    for (let book of library.books) {
+    for (let book of library) {
         createBookCard(book)
     }
 }
@@ -52,14 +56,14 @@ const createBookCard = (book) => {
     const author = document.createElement('p')
     const pages = document.createElement('p')
     const buttonGroup = document.createElement('div')
-    const readBtn = document.createElement('button')
+    const readBtn = document.createElement('p')
     const removeBtn = document.createElement('button')
 
     bookCard.classList.add('book-card')
     buttonGroup.classList.add('button-group')
     readBtn.classList.add('btn')
     removeBtn.classList.add('btn')
-    readBtn.onclick = toggleRead
+    // readBtn.onclick = toggleRead
     removeBtn.onclick = removeBook
 
     title.textContent = `"${book.title}"`
@@ -85,15 +89,66 @@ const createBookCard = (book) => {
 }
 
 const preparePage = () => {
-    myLibrary.forEach(book => {
-        bookCard = createBookCard(book);
-        booksGrid.appendChild(bookCard);
-    });
+    updateBooksGrid();
+}
 
+const openAddBookModal = () => {
+    addBookForm.reset()
+    addBookModal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+const closeAddBookModal = () => {
+    addBookModal.classList.remove('active')
+    overlay.classList.remove('active')
+    errorMsg.classList.remove('active')
+    errorMsg.textContent = ''
+}
+
+
+const closeAllModals = () => {
+    closeAddBookModal()
+    closeAccountModal()
+}
+
+const handleKeyboardInput = (e) => {
+    if (e.key === 'Escape') closeAllModals()
+}
+
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const isRead = document.getElementById('isRead').checked
+    return new Book(title, author, pages, isRead)
+}
+
+const addBook = (e) => {
+    e.preventDefault()
+    const newBook = getBookFromInput()
+
+    library.push(newBook);
+
+    updateBooksGrid()
+
+    closeAddBookModal()
+}
+
+const removeBook = (e) => {
+    const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        ''
+    )
+    library = library.filter((book) => book.title !== title)
+
+    updateBooksGrid()
 }
 
 // events
 
 window.onload = preparePage
-// addBookForm.onsubmit = addBook
-// removeBtn.onclick = removeBook
+//removeBtn.onclick = removeBook
+
+addBookBtn.onclick = openAddBookModal
+overlay.onclick = closeAllModals
+addBookForm.onsubmit = addBook
